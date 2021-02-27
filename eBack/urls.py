4f1b1304e8976bbc6ad20f948cc34891216b1a57
Mahmudul Alam <mahmudula2000@gmail.com>
2021-02-27 23:38:14 +0600

    added home

--- eBack/urls.py
+++ eBack/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('api/', include('api.urls')),
+    path('', lambda x: HttpResponse("Hello world"))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
